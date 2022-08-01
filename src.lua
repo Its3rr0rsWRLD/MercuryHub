022bd5b7439db30266bf43d8ce10cdeda71bbb8f
Error 404 <92172873+ThatError404@users.noreply.github.com>
2022-08-01 12:47:43 -0500

    Update src.lua

--- src.lua
+++ src.lua
@@ -672,8 +672,10 @@ function Library:create(options)
 		AnchorPoint = Vector2.new(0, .5),
 		BackgroundTransparency = 1,
 		TextSize = 14,
+		Text = options.Name,
+		Position = UDim2.new(0, 25, 0.5, 0),
 		TextXAlignment = Enum.TextXAlignment.Left,
-		Size = UDim2.new(1, -45, 0.5, 0),/
+		Size = UDim2.new(1, -45, 0.5, 0),
 		Font = Enum.Font.SourceSans,
 		TextTruncate = Enum.TextTruncate.AtEnd
 	})
