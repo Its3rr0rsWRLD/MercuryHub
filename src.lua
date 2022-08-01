25e085738bec68dcb5069334d7f6c7da978df53a
Error 404 <92172873+ThatError404@users.noreply.github.com>
2022-08-01 12:35:00 -0500

    Update src.lua

--- src.lua
+++ src.lua
@@ -16,6 +16,7 @@ edited: 1/26
 developers:
 v3rm AbstractPoo	discord Abstract#8007
 v3rm 0xDEITY		discord Deity#0228
+v3rm None               discord Error 404#0002
 ]]
 
 local TweenService = game:GetService("TweenService")
@@ -611,7 +612,7 @@ function Library:create(options)
 		Position = UDim2.new(0, 26, 0.5, 0),
 		BackgroundTransparency = 1,
 		Size = UDim2.new(1, -30, .6, 0),
-		Text = options.Link .. "/home",
+		Text = options.Link .. ":Home",
 		Theme = {TextColor3 = "WeakText"},
 		TextSize = 14,
 		TextScaled = false,
@@ -1152,7 +1153,7 @@ function Library:tab(options)
 			selectedTab = tabButton
 			tab.Visible = true
 			tabButton.BackgroundTransparency = 0
-			Library.UrlLabel.Text = Library.Url .. ":" .. options.Name:tolower()
+			Library.UrlLabel.Text = Library.Url .. ":" .. options.Name
 		end)
 
 		quickAccessButton.MouseEnter:connect(function()
@@ -1179,7 +1180,7 @@ function Library:tab(options)
 				selectedTab = tabButton
 				tab.Visible = true
 				tabButton.BackgroundTransparency = 0
-				Library.UrlLabel.Text = Library.Url .. ":" .. options.Name:lower()
+				Library.UrlLabel.Text = Library.Url .. "/" .. options.Name:lower()
 			end
 		end)
 	end
@@ -1242,7 +1243,7 @@ function Library:tab(options)
 			self.homePage.Visible = true
 			self.homeButton:tween{BackgroundTransparency = 0.15}
 			selectedTab = self.homeButton
-			Library.UrlLabel.Text = Library.Url .. "/home"	
+			Library.UrlLabel.Text = Library.Url .. ":Home"	
 		elseif tabButton == lastTab[2] then
 			lastTab = visible[#visible-1]
 			tab.Visible = false
